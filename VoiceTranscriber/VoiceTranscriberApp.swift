2d75174fa0a5c6d800062fb3d795b6a8e8ecc940
Libo Shen <liboooshen@gmail.com>
2024-11-28 16:09:35 +0000

    Initial Commit

---
+++ VoiceTranscriber/VoiceTranscriberApp.swift
@@ -0,0 +1,17 @@
+//
+//  VoiceTranscriberApp.swift
+//  VoiceTranscriber
+//
+//  Created by Libo Shen on 28/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct VoiceTranscriberApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ VoiceTranscriberTests/VoiceTranscriberTests.swift
@@ -0,0 +1,17 @@
+//
+//  VoiceTranscriberTests.swift
+//  VoiceTranscriberTests
+//
+//  Created by Libo Shen on 28/11/2024.
+//
+
+import Testing
+@testable import VoiceTranscriber
+
+struct VoiceTranscriberTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
